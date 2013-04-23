60221868e695ed2d140953db9b3a64f46e00b307
Fredrik Lundgren <k.o.f.lundgren@gmail.com>
2013-04-23 15:17:03 +0200

    CSS Updates

--- Main/Source/Flevolution2/MvcWebRole/App_Start/BundleConfig.cs
+++ Main/Source/Flevolution2/MvcWebRole/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace MvcWebRole
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/Flevolution.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
